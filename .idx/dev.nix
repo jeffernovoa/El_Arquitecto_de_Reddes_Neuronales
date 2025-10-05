fed64c072a91c97e20855935f4354c8d04a1bedd
jefferng35 <jefferng35@gmail.com>
2025-10-05 18:49:11 +0000

    cambios

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,6 +9,9 @@
     # pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.tensorflow
+    pkgs.python311Packages.matplotlib
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
@@ -20,36 +23,5 @@
     extensions = [
       # "vscodevim.vim"
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
   };
 }
